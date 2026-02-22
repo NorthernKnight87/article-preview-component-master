0da3e7cfc5a4ec93868b59a3a513cc8cccdb30e9
James Hewison <jamhew25@gmail.com>
2026-02-22 16:35:40 +0100

    Article Preview Component

--- index.js
+++
@@ -1,6 +0,0 @@
-const shareButton = document.querySelector(".share_button");
-const sharePopup = document.querySelector(".share_popup");
-
-shareButton.addEventListener("click", () => {
-  sharePopup.classList.toggle("hide");
-});
